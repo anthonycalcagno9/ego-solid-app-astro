5b42432efbe960a3569bf9b92b6649f53d46e855
anthonycalcagno9 <acalcagno9@gmail.com>
2025-08-16 21:14:46 -0600

    make bg of carousel images

--- src/components/GameCarousel.tsx
+++ src/components/GameCarousel.tsx
@@ -15,6 +15,7 @@ type Card = {
   hex: string;
   classIconLeft: string;
   classIconRight: string;
+  bgImage: string;
 };
 
 function getPrev(num: number) {
@@ -43,6 +44,7 @@ const gameData = [
     hex: "hover:bg-[#ff0000]",
     classIconLeft: "/images/DisruptorLeft.png",
     classIconRight: "/images/DisruptorRight.png",
+    bgImage: "bg-[url(/images/LavaBG.png)]",
   },
   {
     id: 2,
@@ -51,6 +53,7 @@ const gameData = [
     hex: "hover:bg-[#0080ff]",
     classIconLeft: "/images/TankLeft.png",
     classIconRight: "/images/TankRight.png",
+    bgImage: "bg-[url(/images/BlueBG.png)]",
   },
   {
     id: 3,
@@ -59,6 +62,7 @@ const gameData = [
     hex: "hover:bg-[#80ff00]",
     classIconLeft: "/images/SupportLeft.png",
     classIconRight: "/images/SupportRight.png",
+    bgImage: "bg-[url(/images/SupportBG.png)]",
   },
   {
     id: 4,
@@ -67,6 +71,7 @@ const gameData = [
     hex: "hover:bg-[#8000ff]",
     classIconLeft: "/images/TacticianLeft.png",
     classIconRight: "/images/TacticianRight.png",
+    bgImage: "bg-[url(/images/TacticianBG.png)]",
   },
   {
     id: 5,
@@ -75,6 +80,7 @@ const gameData = [
     hex: "hover:bg-[#fc7e00]",
     classIconLeft: "/images/SpecialistLeft.png",
     classIconRight: "/images/SpecialistRight.png",
+    bgImage: "bg-[url(/images/SpecialistBG.png)]",
   },
 ];
 
@@ -100,22 +106,19 @@ export default function GameCarousel() {
     <div
       className={`w-full min-h-[50vh] relative flex justify-center bg-[#100C07]`}
     >
-      <div className={`w-full lg:w-2/5 z-20 rounded-2xl absolute -top-40`}>
+      <div className={`w-full lg:w-2/5 z-20 absolute -top-40`}>
         <Carousel
           setApi={setApi}
           opts={{ align: "start", loop: true }}
-          className="w-full rounded-2xl"
+          className="w-full"
         >
-          <CarouselContent className="rounded-2xl">
+          <CarouselContent className="">
             {gameData.map((game, index) => (
-              <CarouselItem
-                key={game.id}
-                className={`max-h-[800px] rounded-2xl`}
-              >
+              <CarouselItem key={game.id} className={`max-h-[800px]`}>
                 <div
-                  className={`w-full h-full flex rounded-2xl bg-gradient-to-t ${game.color} from-[25%] to-black`}
+                  className={`w-full h-full flex rounded-2xl ${game.bgImage} bg-center`}
                 >
-                  <div className="w-full flex rounded-2xl justify-center items-center gap-8">
+                  <div className="w-full flex justify-center items-center gap-8">
                     <img
                       src={game.image}
                       className="max-w-full max-h-full px-8 py-8 lg:p-4 object-scale-down"
@@ -127,12 +130,12 @@ export default function GameCarousel() {
           </CarouselContent>
           <CarouselPrevious
             image={gameData[getPrev(currentCard)].classIconLeft}
-            className={`left-1 md:left-6 p-1 absolute ${gameData[getPrev(currentCard)].hex} hover:border hover:border-4 hover:border-black size-12 md:size-22`}
+            className={`left-1 md:left-6 p-1 absolute ${gameData[getPrev(currentCard)].hex} bg-bottom-right cursor-pointer hover:border hover:border-4 hover:border-black size-12 md:size-22`}
             variant="ghost"
           />
           <CarouselNext
             image={gameData[getNext(currentCard)].classIconRight}
-            className={`right-1 md:right-6 p-1 absolute ${gameData[getNext(currentCard)].hex} hover:border hover:border-4 hover:border-black size-12 md:size-22`}
+            className={`right-1 md:right-6 p-1 absolute ${gameData[getNext(currentCard)].hex} cursor-pointer hover:border hover:border-4 hover:border-black size-12 md:size-22`}
             variant="ghost"
           />
         </Carousel>
